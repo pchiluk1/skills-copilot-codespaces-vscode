41484daabe78fffb913e3e4647ff8c7dfeca7d12
pchiluk1 <140013692+pchiluk1@users.noreply.github.com>
2023-07-22 07:48:40 +0000

    File name change to comments.js

---
+++ comments.js
@@ -0,0 +1,26 @@
+//Create Web Server
+//Creaconst express = require('express');
+const app = express();
+//Set up the server port
+const port = 3000;
+//Create a route for the server
+app.get('/', (req, res) => res.send('Hello World!'));
+//Start the server
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+//Create a route for the server
+app.get('/comment', (req, res) => res.send('Hello World!'));
+//Start the server
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+//Create a route for the server
+app.get('/comment', (req, res) => res.send('Hello World!'));
+//Start the server
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+//Create a route for the server
+app.get('/comment', (req, res) => res.send('Hello World!'));
+//Start the server
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+
+
+
+
