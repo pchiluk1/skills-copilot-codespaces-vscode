a257551549c92433e86ffd6907021b4609f7942e
pchiluk1 <140013692+pchiluk1@users.noreply.github.com>
2023-07-22 07:54:59 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,26 +1,23 @@
-//Create Web Server
-//Creaconst express = require('express');
+// Create web server
+// 1. Create a web server that can listen to requests for /hello, and respond with some HTML that says <h1>Hello!</h1>
+// 2. Add /hello/<name> so that the server responds with HTML that says <h1>Hello <name>!</h1>
+// 3. Add /hello/<name>/<year> so that the server responds with HTML that says <h1>Hello <name> born in <year>!</h1>
+
+const express = require('express');
 const app = express();
-//Set up the server port
-const port = 3000;
-//Create a route for the server
-app.get('/', (req, res) => res.send('Hello World!'));
-//Start the server
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-//Create a route for the server
-app.get('/comment', (req, res) => res.send('Hello World!'));
-//Start the server
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-//Create a route for the server
-app.get('/comment', (req, res) => res.send('Hello World!'));
-//Start the server
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-//Create a route for the server
-app.get('/comment', (req, res) => res.send('Hello World!'));
-//Start the server
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
+app.get('/hello', function (req, res) {
+  res.send('<h1>Hello!</h1>');
+});
 
+app.get('/hello/:name', function (req, res) {
+  res.send(`<h1>Hello ${req.params.name}!</h1>`);
+});
 
+app.get('/hello/:name/:year', function (req, res) {
+  res.send(`<h1>Hello ${req.params.name} born in ${req.params.year}!</h1>`);
+});
 
-
+app.listen(3000, function () {
+  console.log('Listening on port 3000');
+});
